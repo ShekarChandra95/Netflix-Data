cf65d5a30b9ddc04a40012e86ed552fa3465d3a0
Chandrashekara C M <83018791+ShekarChandra95@users.noreply.github.com>
2026-04-04 14:55:47 +0530

    Refactor data cleaning and visualization in Analyzer.py

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -3,47 +3,54 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-## Load and basic cleaning
-df = pd.read_csv('Netflix_Movies_and_TV_Shows.csv')
+# Load and basic cleaning
+df = pd.read_csv(r'C:\Users\chand\Downloads\Netflix_Movies_and_TV_Shows.csv')
 
-### 1. Handle Missing Values
-df['country'] = df['country'].fillna('Unknown')
-df['cast'] = df['cast'].fillna('No Data')
-df.dropna(subset=['date_added', 'rating'], inplace=True)
+# 1. Handle Missing Values
+df['Country'] = df['Country'].fillna('Unknown')
+df['Genre'] = df['Genre'].fillna('No Data')
+print(df.columns.tolist())
+df.dropna(subset=['Rating'], inplace=True)
+target_cols = ['date_added', 'Rating']
+# This line keeps only the columns that are actually in df.columns
+existing_cols = [c for c in target_cols if c in df.columns]
 
-### 2. Fix Date Formatting
-df['date_added'] = pd.to_datetime(df['date_added'].str.strip())
-df['year_added'] = df['date_added'].dt.year
-df['month_added'] = df['date_added'].dt.month_name()
+df.dropna(subset=existing_cols, inplace=True)
+
+# 2. Fix Date Formatting
+df['Release Year'] = pd.to_datetime(df['Release Year'].astype(str).str.strip(), format='%Y')
+df['year_added'] = df['Release Year'].dt.year
+df['month_added'] = df['Release Year'].dt.month_name()
 
 #Phase 2: Core EDA & Visualizations
-## 1. The Content Split (Movies vs. TV Shows)
-plt.figure(figsize=(7, 5))
-sns.countplot(x='type', data=df, palette=['#e50914', '#221f1f'])
+# 1. The Content Split (Movies vs. TV Shows)
+plt.figure(figsize=(3, 2))
+sns.countplot(x='Type', data=df, hue='Type', palette=["#1fe509", "#2626bd"], legend=False)
 plt.title('Distribution of Movies vs TV Shows')
 plt.show()
 
 ## 2. Content Growth Over Time
-content_by_year = df.groupby(['year_added', 'type']).size().unstack().fillna(0)
-content_by_year.plot(kind='line', figsize=(12, 6), color=['#e50914', '#b20710'])
+content_by_year = df.groupby(['year_added', 'Type']).size().unstack().fillna(0)
+content_by_year.plot(kind='line', figsize=(12, 6), color=["#2c0c95", '#b20710'])
 plt.title('Content Added Over the Years')
 plt.ylabel('Number of Titles')
+plt.show()
 
 ## 3. Top 10 Countries producing Content
 
 # Explode the country column for accurate counts
-countries = df['country'].str.split(', ').explode().value_counts().head(10)
+countries = df['Country'].str.split(', ').explode().value_counts().head(10)
 
 plt.figure(figsize=(12,6))
-sns.barplot(x=countries.values, y=countries.index, palette='Reds_r')
+sns.barplot(x=countries.values, y=countries.index, hue=countries.index, palette='Reds_r', legend=False)
 plt.title('Top 10 Content Producing Countries')
+plt.show()
 
 # Filter top 10 countries and cross-tabulate with ratings
-top_countries_list = df['country'].value_counts().head(10).index
-subset = df[df['country'].isin(top_countries_list)]
+top_countries_list = df['Country'].value_counts().head(10).index
+subset = df[df['Country'].isin(top_countries_list)]
 
-rating_map = pd.crosstab(subset['country'], subset['rating'])
+rating_map = pd.crosstab(subset['Country'], subset['Rating'])
 sns.heatmap(rating_map, annot=True, cmap='Reds', fmt='d')
 plt.title('Content Rating Distribution by Country')
-
-# 3. Top 10 Countries producing Content
+plt.show()
